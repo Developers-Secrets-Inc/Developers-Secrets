669620a9894e4983a574c2cf0de650308a4ee351
AliiiBenn <davidddpereiraaa6@gmail.com>
2025-05-21 15:25:08 +0200

    feat: Remove example stories and configure Tailwind CSS and themes addon

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,27 @@
 import type { Preview } from '@storybook/react'
+import '../src/styles/globals.css'
+import { withThemeByDataAttribute } from '@storybook/addon-themes'
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
-};
+}
 
-export default preview;
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+    attributeName: 'data-mode',
+  }),
+]
+
+export default preview
